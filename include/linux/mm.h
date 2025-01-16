d961949f36a1120b237bdcb3b48023f5c334681d
yyq <3531236823@qq.com>
2025-01-16 14:48:45 +0800

    fix: build error

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -546,6 +546,20 @@ static inline void page_mapcount_reset(struct page *page)
 }
 
 int __page_mapcount(struct page *page);
+extern void *kvmalloc_node(size_t size, gfp_t flags, int node);
+static inline void *kvmalloc(size_t size, gfp_t flags)
+{
+	return kvmalloc_node(size, flags, NUMA_NO_NODE);
+}
+static inline void *kvzalloc_node(size_t size, gfp_t flags, int node)
+{
+	return kvmalloc_node(size, flags | __GFP_ZERO, node);
+}
+static inline void *kvzalloc(size_t size, gfp_t flags)
+{
+	return kvmalloc(size, flags | __GFP_ZERO);
+}
+extern void kvfree(const void *addr);
 
 /*
  * Mapcount of 0-order page; when compound sub-page, includes
